c739c5e345778ca3061a1dd656adfb17e1547d34
joshNic <256jomu@gmail.com>
2025-09-24 10:27:23 -0500

    add dtos

--- src/main/java/com/customerrewards/dto/CustomerRewardsResponse.java
+++ src/main/java/com/customerrewards/dto/CustomerRewardsResponse.java
@@ -1,4 +1,32 @@
 package com.customerrewards.dto;
 
+import java.util.Map;
+
 public class CustomerRewardsResponse {
+    private Long customerId;
+    private String customerName;
+    private Map<String, Integer> monthlyPoints;
+    private Integer totalPoints;
+
+    public CustomerRewardsResponse() {}
+
+    public CustomerRewardsResponse(Long customerId, String customerName, Map<String, Integer> monthlyPoints, Integer totalPoints) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.monthlyPoints = monthlyPoints;
+        this.totalPoints = totalPoints;
+    }
+
+
+    public Long getCustomerId() { return customerId; }
+    public void setCustomerId(Long customerId) { this.customerId = customerId; }
+
+    public String getCustomerName() { return customerName; }
+    public void setCustomerName(String customerName) { this.customerName = customerName; }
+
+    public Map<String, Integer> getMonthlyPoints() { return monthlyPoints; }
+    public void setMonthlyPoints(Map<String, Integer> monthlyPoints) { this.monthlyPoints = monthlyPoints; }
+
+    public Integer getTotalPoints() { return totalPoints; }
+    public void setTotalPoints(Integer totalPoints) { this.totalPoints = totalPoints; }
 }
