261ef9ce8606c0b059232a60903bb2b39cafc960
joshNic <256jomu@gmail.com>
2025-09-24 10:51:14 -0500

    add transaction and customer repositories

---
+++ src/main/java/com/customerrewards/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.customerrewards.repository;
+
+import com.customerrewards.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
